9c420a1a6505bba8b76d6f8309859de6847a4bf4
ToKyun02 <developtk02@gmail.com>
2026-07-24 15:37:51 +0900

    feat : add to admin guard

---
+++ server/src/auth/guards/admin.guard.ts
@@ -0,0 +1,28 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { PrismaService } from '../../prisma/prisma.service';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const req = context.switchToHttp().getRequest();
+
+    const sub = req.user?.sub;
+    if (!sub) throw new UnauthorizedException();
+
+    const user = await this.prisma.user.findUnique({
+      where: { id: sub },
+      select: { role: true },
+    });
+
+    if (user?.role !== 'ADMIN') throw new ForbiddenException();
+    return true;
+  }
+}
