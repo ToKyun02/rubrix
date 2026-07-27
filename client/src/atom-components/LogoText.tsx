85a9b02b082c6825aeca58afa1f21205bd0754a6
ToKyun02 <developtk02@gmail.com>
2026-07-27 11:26:41 +0900

    chore : add to default color for LogoText

--- client/src/atom-components/LogoText.tsx
+++ client/src/atom-components/LogoText.tsx
@@ -5,7 +5,10 @@ interface LogoTextProps extends HTMLAttributes<HTMLSpanElement> {}
 
 export default function LogoText({ className, ...props }: LogoTextProps) {
   return (
-    <span className={cn('text-lg font-extrabold', className)} {...props}>
+    <span
+      className={cn('text-heading text-lg font-extrabold', className)}
+      {...props}
+    >
       <span>rubrix</span>
       <span className="text-blue">_</span>
     </span>
