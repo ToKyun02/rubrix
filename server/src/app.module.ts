b8abc2cde55453fc42e9db29436edcb24732edf2
Kim To Kyun <58878190+ToKyun02@users.noreply.github.com>
2026-07-30 11:49:14 +0900

    [ISSUE : #69] docker compose 의존성 수정 (#70)

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { AssignmentModule } from './assignment/assignment.module';
 import { AuthModule } from './auth/auth.module';
 import { GithubAppModule } from './github-app/github-app.module';
+import { HealthModule } from './health/health.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { RepoModule } from './repo/repo.module';
 import { SubmissionModule } from './submission/submission.module';
@@ -16,6 +17,7 @@ import { SubmissionModule } from './submission/submission.module';
     AssignmentModule,
     RepoModule,
     SubmissionModule,
+    HealthModule,
   ],
 })
 export class AppModule {}
