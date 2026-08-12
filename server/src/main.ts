af675d3b5b4ee00e066183261a3937e795fc1e57
ToKyun02 <developtk02@gmail.com>
2026-08-12 16:30:01 +0900

    feat : swagger 연동

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,11 +1,23 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import cookieParser from 'cookie-parser';
 import 'dotenv/config';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { rawBody: true });
+
+  const config = new DocumentBuilder()
+    .setTitle('Rubrix API Example')
+    .setDescription('The Rubrix API description')
+    .setVersion('1.0')
+    .addTag('rubrix')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, documentFactory);
+
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
   app.use(cookieParser());
   app.enableCors({ origin: process.env.CLIENT_URL, credentials: true });
